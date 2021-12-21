7208f608a9d8d9a0cc7786a28f27c0f850cabffe
Ami_parekh1594 <amiparekh87@gmail.com>
2021-12-21 16:46:17 -0500

    pyramid of numbers start from 1,2,....

---
+++ PatternPrograms/NumbersPattern/NumbersWithoutReassigning.java
@@ -0,0 +1,23 @@
+package PatternPrograms.NumbersPattern;
+
+import java.util.Scanner;
+
+public class NumbersWithoutReassigning {
+    public static void main(String[] args) {
+        int i, j, n;
+        int num = 1;
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter no. of rows: ");
+        n = sc.nextInt();
+
+        for (i = 1; i <= n; i++) {
+            for (j = 1; j <= i; j++) {
+                System.out.print(num + " ");
+                num = num + 1;
+            }
+            System.out.println("");
+        }
+        sc.close();
+    }
+}
